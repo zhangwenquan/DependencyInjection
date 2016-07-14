07fa72e7e438d67c7767431add3f07795206a7bb
Pavel Krymets <pavel@krymets.com>
2016-07-14 11:31:41 -0700

    WIP

---
+++ test/Microsoft.Extensions.DependencyInjection.Simple.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection.Specification;
+
+namespace Microsoft.Extensions.DependencyInjection.Simple.Tests
+{
+    public class ServiceProviderContainerTests : DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection collection) =>
+            new ServiceProvider(collection);
+    }
+}
