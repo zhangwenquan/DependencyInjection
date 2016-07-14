4f079f14e1397ac8c6259dccc08058affdf7bdde
Pavel Krymets <pavel@krymets.com>
2016-07-14 10:44:54 -0700

    WIP

---
+++ src/Microsoft.Extensions.DependencyInjection.Simple/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection.Simple
+{
+    public class ServiceProvider: IServiceProvider
+    {
+        private readonly ServiceDescriptor[] _services;
+
+        public ServiceProvider(IEnumerable<ServiceDescriptor> services)
+        {
+            _services = services.ToArray();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            _services.Where()
+        }
+
+        private void 
+    }
+}
